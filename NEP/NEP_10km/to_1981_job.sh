3050117a5fe754a7d4db95e9d8996e246797869b
amoebaliz <liz.drenkard@gmail.com>
2020-12-28 12:22:34 -0500

    NEP_10km files added to repo

---
+++ NEP/NEP_10km/to_1981_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks=360
+#SBATCH --clusters=c4
+#SBATCH --time=20
+#SBATCH --job-name=NEP_soda
+
+cd /lustre/f2/scratch/gfdl/Liz.Drenkard/NEP
+
+export y=1980
+
+srun ./MOM6
+
+if [ $? -eq 0 ]; then
+    cp -r RESTART restarts_$y
+    cp MOM_IC.nc restarts_$y
+else
+    exit 1
+fi
+
